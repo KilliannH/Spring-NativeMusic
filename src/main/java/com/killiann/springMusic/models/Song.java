52a12fc6697a8a6721668e6125a8a6044a31d247
KilliannH <killiannhervagault@gmail.com>
2020-11-27 18:21:49 +0100

    bugfix

--- src/main/java/com/killiann/springMusic/models/Song.java
+++ src/main/java/com/killiann/springMusic/models/Song.java
@@ -43,6 +43,7 @@ public class Song {
                 ", title='" + title + '\'' +
                 ", filename='" + filename + '\'' +
                 ", artists='" + artists + '\'' +
+                ", albums='" + albums + '\'' +
                 '}';
     }
 
