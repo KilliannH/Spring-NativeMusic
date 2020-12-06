90d30ac225480eaaf1050ef9b570eea4a5efbbab
KilliannH <killiannhervagault@gmail.com>
2020-12-06 22:00:06 +0100

    removed jwt lib as it doesn't compile with tomcat

--- src/main/java/com/killiann/springMusic/config/SpringSecurityConfig.java
+++ src/main/java/com/killiann/springMusic/config/SpringSecurityConfig.java
@@ -1,19 +1,15 @@
 package com.killiann.springMusic.config;
 
-import com.killiann.springMusic.filters.JwtRequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.sql.DataSource;
 
@@ -22,15 +18,6 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     DataSource dataSource;
-
-    @Autowired
-    private JwtRequestFilter jwtRequestFilter;
-
-    @Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -50,13 +37,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                //HTTP Basic authentication
+                //HTTP Disabled authentication
         .csrf().disable()
-                .authorizeRequests().antMatchers("/authenticate/**").permitAll()
-                .anyRequest().hasRole("USER")
-                .and()
-                .exceptionHandling().and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+                .authorizeRequests().anyRequest().permitAll();
     }
 }
